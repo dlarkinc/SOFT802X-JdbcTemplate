d068ae6dc7adc61485763e39982e3ef9c269f4b0
Larkin Cunningham <larkin.cunningham@cit.ie>
2017-09-27 16:53:05 +0100

    Tutorial Doc 1 code

---
+++ src/main/java/ie/cit/soft8027/jdbctemplateproject/domain/Movement.java
@@ -0,0 +1,29 @@
+package ie.cit.soft8027.jdbctemplateproject.domain;
+
+public class Movement {
+
+	private int id;
+	
+	private String name;
+
+	@Override
+	public String toString() {
+		return "Movement [id=" + id + ", name=" + name + "]";
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
